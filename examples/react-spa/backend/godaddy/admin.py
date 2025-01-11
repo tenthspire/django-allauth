dc42bd84df6e5bac980f3fb3e0ef3608d672a85d
anny9229 <aniketsanchaniya9229@gmail.com>
2025-01-11 15:53:48 +0530

    GoDaddy Commit

--- examples/react-spa/backend/godaddy/admin.py
+++ examples/react-spa/backend/godaddy/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import DomainPuchase
+from .models import DomainPuchase,UpdatedDomainDetail
 
 @admin.register(DomainPuchase)
 class DomainPurchaseAdmin(admin.ModelAdmin):
     list_display = ('domain_name', 'purchse_date', 'status')
+
+@admin.register(UpdatedDomainDetail)
+class DomainUpdateAdmin(admin.ModelAdmin):
+    list_display = ('domain_name',  'updated_at')
